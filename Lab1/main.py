e6047644acea2e123d1ce897f55d77d6632afa5c
Alex <alexxkolcc@gmail.com>
2020-02-24 01:05:38 +0300

    Lab1. 1 and 2 done.

---
+++ Lab1/main.py
@@ -0,0 +1,91 @@
+import random
+import time
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+#import statistics as st
+from statistics import Statistics
+
+
+def build_graphics(data, n_bins=10):
+    # x_min = np.min(data)
+    # x_max = np.max(data) + 0.3
+    # l = 10
+    # delta = (x_max - x_min) / l
+    # n = len(data)
+    #
+    # si = [x_min]
+    # pi = []
+    # for i in range(l):
+    #     si.append(si[-1] + delta)
+    #     pi.append(np.count_nonzero(np.greater_equal(data, si[i]) == np.less(data, si[i + 1])))
+    #     print(si[i], si[i+1], pi[i])
+
+    fig, (ax1, ax2) = plt.subplots(
+        nrows=2, ncols=1,
+        figsize=(8, 10)
+    )
+    ax1.set_title("Histogram")
+    ax1.set_ylabel("Frequency * " + str(len(data)))
+    ax1.set_xlabel("Value")
+    ax1.hist(data, bins=n_bins, color='#539caf', edgecolor='black')
+    ax1.grid(axis='y', linestyle=':', linewidth='0.5')
+
+    ax2.set_title("Empirical distribution function graph")
+    ax2.set_ylabel("F(x)")
+    ax2.set_xlabel("x")
+    ax2.grid(axis='y', linestyle=':', linewidth='0.5')
+    value, count = [], []
+    for x in np.unique(data):
+        value.append(x)
+        count.append(np.count_nonzero(np.less_equal(data, x) == True))
+
+    first_last = 0.5  # для первой и последней линии
+    for i in range(len(value)):
+        if i == 0:
+            x = np.linspace(value[i] - first_last, value[i], 2)
+        elif i == len(value) - 1:
+            x = np.linspace(value[i], value[i] + first_last, 2)
+        else:
+            x = np.linspace(value[i - 1], value[i], 2)
+        y = [count[i] / len(data), count[i] / len(data)]
+        ax2.plot(
+            x, y,
+            linewidth=1,
+            color='blue'
+        )
+    # levels = np.linspace(0, 1, len(data) + 1)  # endpoint 1 is included by default
+    # ax2.step(sorted(list(data) + [max(data)]), levels)
+    # plt.show()
+    fig.savefig("graphics.png")
+
+
+def main():
+    stat = Statistics()
+    stat.load_data("data.txt")
+
+    build_graphics(stat.get_data())
+    print("Среднее значение выборки: ", stat.find_average_sample_value())
+    print("Выборочная дисперсия: ", stat.find_selective_dispersion())
+    print("Стандартная ошибка: ", stat.find_standard_error())
+    print("Мода: ", stat.find_mode())
+    print("Медиана: ", stat.find_median())
+    print("Квартили: ", (stat.find_quartiles()))
+    #print("Квартили: ", (stat.find_quartiles()))
+    print("Стандартное отклонени: ", (stat.find_standard_deviation()))
+    print("Эксцесс: ", (stat.find_kurtosis()))
+    print("Асимметричность: ", (stat.find_skewness()))
+    print("Минимум: ", (stat.find_min()))
+    print("Максимум: ", (stat.find_max()))
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
---
+++ Lab1/statistics.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+
+class Statistics:
+    """
+        Класс для нахождения всяких статистических величин.
+
+    """
+
+    def __init__(self, ):
+        self.data = []
+        self.n = 0
+
+    def load_data(self, file):
+        """Метод для загрузки данных из файла."""
+        with open(file) as f:
+            for line in f:
+                for x in line.split():
+                    self.data.append(float(x))
+        self.n = len(self.data)
+        self.data = np.sort(self.data)
+
+    def get_data(self):
+        return self.data
+
+    # среднее значение выборки
+    def find_average_sample_value(self):
+        """Метод для вычисления среднего значения выборки."""
+        return np.sum(self.data) / self.n
+
+    # Выборочная дисперсия
+    def find_selective_dispersion(self):
+        """Метод для вычисления выборочной дисперсии."""
+        return np.sum(np.array(self.data) ** 2) / self.n - self.find_average_sample_value() ** 2
+
+    # стандартная ошибка
+    def find_standard_error(self):
+        """Метод для вычисления стандартной ошибки."""
+        return (self.find_selective_dispersion() * self.n
+                / (self.n - 1)) / np.sqrt(self.n)
+
+    # мода
+    def find_mode(self):
+        """Метод для вычисления моды."""
+        value, count = np.unique(self.data, return_counts=True)
+        return value[np.argmax(count)]
+
+    # медиана
+    def find_median(self, data=[]):
+        """Метод для вычисления медианы."""
+        if len(data) == 0:
+            data = self.data
+            n = self.n
+        else:
+            n = len(data)
+        median = data[n // 2]
+        if n % 2 == 0:
+            median += data[n // 2 - 1]
+            median /= 2
+        return median
+
+    # 1, 2, 3 квартили
+    def find_quartiles(self):
+        """Метод для вычисления квартилей."""
+        data1 = self.data[:self.n // 2]
+        data2 = self.data[self.n // 2:]
+        q1 = self.find_median(data1)
+        q2 = self.find_median()
+        q3 = self.find_median(data2)
+        return q1, q2, q3
+
+    # ящик с усами
+    def find_box_plot(self):
+        """Метод для вычисления ящика с усами."""
+        q1, _, q3 = self.find_quartiles()
+        k = 1.5  # коэффициент, наиболее часто употребляемое значение которого равно 1,5
+        X1 = q1 - k * (q3 - q1)
+        X2 = q3 + k * (q3 - q1)
+        ## це график
+        return 0
+
+    # стандартное отклонение
+    def find_standard_deviation(self):
+        """Метод для вычисления стандартного отклонения."""
+        asv = self.find_average_sample_value()
+        return np.sqrt(
+            np.sum(np.array(self.data - asv) ** 2) / (self.n - 1)
+        )
+
+    # эксцесс
+    def find_kurtosis(self):
+        """Метод для вычисления коэффициента эксцесса."""
+        asv = self.find_average_sample_value()
+        return np.sqrt(
+            np.sum(np.array(self.data - asv) ** 4) / self.n
+        )
+
+    # Асимметричность
+    def find_skewness(self):  # ????
+        """Метод для вычисления коэффициента ассиметричности."""
+        asv = self.find_average_sample_value()
+        skewness = (np.sum(np.array(self.data - asv) ** 3) / self.n) / (self.find_selective_dispersion() ** 3)
+        result = "распределение "
+
+        if np.abs(skewness) < 0.25:
+            result += "незначительно"
+        elif np.abs(skewness) > 0.5:
+            result += "существенно "
+        else:
+            result += "умеренно "
+
+        if skewness > 0:
+            result += " скошено вправо"
+        elif skewness < 0:
+            result += " скошено влево"
+        return skewness, result
+
+    # минимум
+    def find_min(self):
+        """Метод для нахождения мининума."""
+        return np.min(self.data)
+
+    # максимум
+    def find_max(self):
+        """Метод для нахождения максимума."""
+        return np.max(self.data)
